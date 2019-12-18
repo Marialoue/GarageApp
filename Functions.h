b4ca54fe94f8dd3661d6c74375939e63c1fbd7e8
Maria Karlsson <mariahkarlsson_@hotmail.com>
2019-12-18 19:54:04 +0100

    adding working constructor and destructor

--- Functions.h
+++ Functions.h
@@ -1,39 +1,79 @@
-/*  Functions.h */
+/*  Functions.h header file to Main.cpp in GarageApp */
 
-#pragma once
+#pragma once //the compiler will include the header file only once when compiling the source code
+using namespace std;
 
-//This is a representation of the building "Garage" where we store all vehicles, later on we can add other variables that effect the building, e.g air pollution
-class Garage{ 
+//This is the building "Garage" where we store all vehicles, later on we can add other variables that effect the building, e.g air pollution
+class Garage{
 protected:
-    int wheels; 
-    int year;
-    std::string color;
-    std::string registrationNo; //not needed for bicycles
-    std::string brand;
-    std::string model; 
-
+    int x; //x no of vehicles
+    int counter; //for adding and removing vehicles
+    int maxParkingSpaces; //setting maximum no of parking spaces
 public:
-    std::vector<Garage> Vehicles; //vector with type garage class and name Vehicle
-    virtual std::string output(std::string color){
-        return color;
+    
+    virtual void printProperties() = 0; //function to print properties to be used in all subclasses
+
+    vector<Garage *> GarageVehicles; //we will store all vehicles here, there will be a fixed no of spaces available
+    
+    void vehicleIndex(){ 
+        cout << "There are " << GarageVehicles.size() << " vehicles in the garage." << endl;
     }
-};
+
+    void listOfVehicles(){
+        cout << "These are the vehicles in the garage right now:" << endl;
+        for (int i = 0; i < GarageVehicles.size(); i++){
+            cout << GarageVehicles[i] << endl;
+            }
+    }
+
+    void addVehicle(Garage* x){  //add vehicle to list of vehicles
+        GarageVehicles.push_back(x);
+        counter++;
+    }
+
+    void removeVehicle(Garage* x){ //remove vehicle from list of vehicles
+        //GarageVehicles.erase(x);
+        counter--;
+    }
+
+    Garage(){
+        cout << "Garage constructor" << endl;
+    } //constructor
+    
+    ~Garage(){
+        cout << "Garage destructor" << endl;
+    } //destrctor
+}; 
 
 //All vehicles will inherit this class
-class Vehicle{ 
+class Vehicle : public Garage{ 
 protected:
+    int noOfWheels;
+    int year;
+    string color;
+    string registrationNo;
+    string brand;
+    string model;
+    string insurranceNo; 
+
 public:
-    Vehicle(){ //default constructor
-    }
 
-    ~Vehicle(){ //destructor to be used when we delete our objects
-    }
+    Vehicle(string x, string y, int z) : registrationNo(x), color(y), noOfWheels(z) {
+        cout << "Vehicle constructor" << endl;
+    } //constructor
+    ~Vehicle(){
+        cout << "Vehicle destructor" << endl;
+    } //destructor
 
+    virtual void printProperties(){}   
 };
 
 //Function that points to vehicle class that makes a new vehicle
-Vehicle* makeNewVehicle(){
-    Vehicle *newVehicle = new Vehicle;
-    return newVehicle;
-    //return (new Vehicle); (same as above, only compressed)
-}
+/* Vehicle* makeNewVehicle(){
+    Vehicle* newVehicle = new Vehicle;
+        return newVehicle;
+    //new vehicle with no properties
+    //return (new newVehicle); (same as above, only compressed)
+}*/
+
+/* use counter in for loop when adding or removing vehicle */
